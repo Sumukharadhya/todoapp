9d732d78e77c443c1428a8a53262d62333aa2cdb
Sumukh <sumukharadhyakj07@gmail.com>
2022-09-30 23:33:18 +0530

    Initial Commit

---
+++ todoapp/todoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoappApp.swift
+//  todoapp
+//
+//  Created by Sumukh Kj on 30/09/22.
+//
+
+import SwiftUI
+
+@main
+struct todoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
